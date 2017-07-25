e16aecf23502738bb501eb623fc53cfd0816eeba
Lauviah <priyankakishnee@gmail.com>
2017-07-25 16:06:44 +0400

    My Second Practice
    A circle bouncing against the innerWidth and innerHeight

--- canvas.js
+++ canvas.js
@@ -1,32 +1,41 @@
-
-/*$$$$$$$$$  FIRST PRACTICE  $$$$$$$$ */
-
-
 var canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var c = canvas.getContext('2d');
+var ctx = canvas.getContext('2d');
 
-//Switzerland Flag
-c.fillStyle = 'red';
-c.fillRect(100, 50, 400, 200);
-c.fillStyle = 'white';
-c.fillRect(200, 110, 200, 70);
-c.fillStyle = 'white';
-c.fillRect(265, 70, 70, 150);
+		//Declaration of all variable
+		var x = Math.random() * innerWidth;
+		var y = Math.random() * innerHeight;
+		var dx = (Math.random() - 0.5) * 8;
+		var dy = (Math.random() -0.5) * 8;
+		var r = 50;
 
+function animate(){
+	requestAnimationFrame(animate)
+		                                     //r= radius
 
-// Mauritius Flag
-c.fillStyle = 'red';
-c.fillRect(800, 50, 400, 50);
-c.fillStyle = 'blue';
-c.fillRect(800, 100, 400, 50);
-c.fillStyle = 'yellow';
-c.fillRect(800, 150, 400, 50);
-c.fillStyle = 'green';
-c.fillRect(800, 200, 400, 50);
+		ctx.beginPath();
+		//ctx.clearRect(0, 0, innerWidth, innerHeight);       unable comment to make only 1 bouncing ball
+		ctx.arc(x, y, r, Math.PI*2, false);
+		ctx.fillStyle ='pink';
+		ctx.fill();
+
+		if(x + r > innerWidth || x - r < 0){
+
+			dx = -dx;
+		}
+
+		if(y + r > innerHeight || y - r < 0){
+			dy = -dy;
+		}
+
+		x += dx;
+		y += dy;
+	
+}
+animate();
 
 
 
